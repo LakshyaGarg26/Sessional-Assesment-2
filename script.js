6d5dfe621c734060fdb53fcd15b58bd92abc1447
LakshyaGarg26 <lakshya0418.be20@chitkara.edu.in>
2022-08-11 22:23:29 +0530

    Data edit

--- script.js
+++ script.js
@@ -1,13 +1,11 @@
 var responseElem = document.getElementById("displayData");
 
-
 var url;
 
-
 function get() {
-   
   var xhr = new XMLHttpRequest();
-  url = "https://jsonplaceholder.typicode.com/users?utm_source=Mailerlite&utm_medium=E-mail&utm_campaign=Test%20Series&utm_term=2022-08-09"
+  url =
+    "https://jsonplaceholder.typicode.com/users?utm_source=Mailerlite&utm_medium=E-mail&utm_campaign=Test%20Series&utm_term=2022-08-09";
   xhr.open("GET", url, true);
   xhr.onreadystatechange = callback;
   xhr.send();
@@ -15,39 +13,55 @@ function get() {
   function callback() {
     if (xhr.readyState == 4 && xhr.status == 200) {
       var response = JSON.parse(xhr.responseText);
-      for(var i=0;i<response.length;i++)
-      {
-        
-     var item = document.createElement('li');
-     var info = document.createElement('div');
-     info.setAttribute('class','info');
-     item.appendChild(info);
-     var name = document.createElement('p');
-     name.setAttribute('class','name');
-     var username = document.createElement('p');
-     username.setAttribute('class','username');
-     var email = document.createElement('p');
-     email.setAttribute('class','email');
-     var companyName = document.createElement('p');
-     companyName.setAttribute('class','companyName');
-     name.innerHTML = "Name : "+response[i].name;
-     username.innerHTML = "Username : "+response[i].username;
-     email.innerHTML = "Email : "+response[i].email;
-     companyName.innerHTML = "Company Name : "+response[i].company.name;
-     info.appendChild(name);
-     info.appendChild(username);
-     info.appendChild(email);
-     info.appendChild(companyName);
-    
-     console.log(item);
-     document.getElementById('displayData').appendChild(item);
-    
+      for (var i = 0; i < response.length; i++) {
+        var item = document.createElement("li");
+        var info = document.createElement("div");
+        info.setAttribute("class", "info");
+        item.appendChild(info);
+        var name = document.createElement("p");
+        name.setAttribute("class", "name");
+        var username = document.createElement("p");
+        username.setAttribute("class", "username");
+        var email = document.createElement("p");
+        email.setAttribute("class", "email");
+        var companyName = document.createElement("p");
+        companyName.setAttribute("class", "companyName");
+        var phone = document.createElement("p");
+        phone.setAttribute("class", "phone");
+        var website = document.createElement("p");
+        website.setAttribute("class", "website");
+
+        var address = document.createElement("p");
+        address.setAttribute("class", "address");
+
+        name.innerHTML = "Name : " + response[i].name;
+        username.innerHTML = "Username : " + response[i].username;
+        email.innerHTML = "Email : " + response[i].email;
+        companyName.innerHTML = "Company Name : " + response[i].company.name;
+        phone.innerHTML = "Phone : " + response[i].phone;
+        website.innerHTML = "Website : " + response[i].website;
+
+        address.innerHTML =
+          "Address : " +
+          response[i].address.suite +
+          "," +
+          response[i].address.street +
+          "," +
+          response[i].address.city;
+
+        info.appendChild(name);
+        info.appendChild(username);
+        info.appendChild(email);
+        info.appendChild(companyName);
+        info.appendChild(phone);
+        info.appendChild(website);
+        info.appendChild(address);
+
+        console.log(item);
+        document.getElementById("displayData").appendChild(item);
       }
-      
-      
-    //   console.log(persons);
-    //   responseElem.innerHTML = JSON.stringify(persons, null, 2);
-    // responseElem.innerHTML = persons;
+
+     
     } else {
       console.log(xhr.statusText);
       responseElem.innerHTML = xhr.statusText;
